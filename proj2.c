f1378057414ded1fd0cfb181f94054a8846045db
PeterVinarcik <peter.vinarcik99@gmail.com>
2019-04-20 00:49:18 +0200

    ADDED MOLO AND VISUALIZED CODE

--- proj2.c
+++ proj2.c
@@ -1,3 +1,12 @@
+///////////////////////////////////////////////////////////////////////////
+///                              IOS Project 2                          ///
+///                                 proj2.c                             ///
+///                              VUT FIT BRNO                           ///
+///                                20.4.2019                            ///
+///                        xvinar00@stud.fit.vutbr.cz                   ///
+///                             Peter Vinarcik                          ///
+///////////////////////////////////////////////////////////////////////////
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -21,8 +30,8 @@
 //                FUNCTIONS              //
 //***************************************//
 int control(int argc, char *argv[]);
-void proc1();
-void proc2();
+void hack1();
+void serf1();
 
 //***************************************//
 //       DEFINES && GLOBAL VARIABLES     //
@@ -33,6 +42,7 @@ sem_t *semaphore2;
 #define shmKEY "/ios-proj2"
 #define shmHACK "/ios-proj2-hack"
 #define shmSERF "/ios-proj2-serf"
+#define shmMOLO "/ios-proj2-molo"
 
 int main(int argc, char *argv[])
 {
@@ -43,6 +53,11 @@ int main(int argc, char *argv[])
     if (controler == -1){
         return -1;
     }
+
+
+    /**************************************************************************************/
+    /**                                      SEMAPHORES                                  **/
+    /**************************************************************************************/
 
     // create two locked semaphores
     if ((semaphore1 = sem_open(SEMAPHORE1_NAME, O_CREAT | O_EXCL, 0666, 0)) == SEM_FAILED)
@@ -59,44 +74,51 @@ int main(int argc, char *argv[])
     }
     sem_close(semaphore2);
 
-    int pidSerf;
-    int pidHack;
-    int *shm;
-    int *hack;
-    int *serf;
-    int shmID;
-    int hackID;
-    int serfID;
-    pid_t consPID;
-    pid_t prodPID;
+    /**************************************************************************************/
+    /**                                    SHARED MEMORY                                 **/
+    /**************************************************************************************/
 
-    shmID = shm_open(shmKEY, O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR);
+    int pidSerf, pidHack;
+
+    int *shm, *hack, *serf, *molo;
+
+    int shmID, hackID, serfID, moloID;
+
+    pid_t consPID, prodPID;
+
+    shmID = shm_open(shmKEY, O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR);      //GLOBAL INDEXING FOR PROCESS
     ftruncate(shmID, shmSIZE);
-    hackID = shm_open(shmHACK, O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR);
+    hackID = shm_open(shmHACK, O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR);    //GLOBAL INDEXING FOR HACKERS
     ftruncate(hackID, shmSIZE);
-    serfID = shm_open(shmSERF, O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR);
+    serfID = shm_open(shmSERF, O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR);    //GLOBAL INDEXING FOR SERFS
     ftruncate(serfID, shmSIZE);
-    shm = (int*)mmap(NULL, shmSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shmID, 0);
-    hack = (int*)mmap(NULL, shmSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, hackID, 0);
-    serf = (int*)mmap(NULL, shmSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, serfID, 0);
+    moloID = shm_open(shmMOLO, O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR);    //GLOBAL INDEXING FOR MOLO CAPACITY
+    ftruncate(moloID, shmSIZE);
+    shm = (int*)mmap(NULL, shmSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shmID, 0);      //MMAP shm
+    hack = (int*)mmap(NULL, shmSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, hackID, 0);    //MMAP hack
+    serf = (int*)mmap(NULL, shmSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, serfID, 0);    //MMAP serf
+    molo = (int*)mmap(NULL, shmSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, moloID, 0);    //MMAP molo
     close(shmID);
     close(hackID);
     close(serfID);
+    close(moloID);
     *shm = 0;
     *hack = 0;
     *serf = 0;
+    *molo = 0;
     munmap(shm, shmSIZE);
     munmap(hack, shmSIZE);
     munmap(serf, shmSIZE);
+    munmap(molo, shmSIZE);
 
-
-
-
+    /**************************************************************************************/
+    /**                             2 MAIN PROCESSES [SERF/HACKER]                       **/
+    /**************************************************************************************/
 
     pidHack = fork();
     //HACKER PROCES
     if (pidHack == 0) { // child
-        proc1();
+        hack1();
         exit(0);
     } else if (pidHack > 0) consPID = pidHack;
     else{
@@ -107,7 +129,7 @@ int main(int argc, char *argv[])
     //SERF PROCES
     pidSerf = fork();
     if (pidSerf == 0) { // child
-        proc2();
+        serf1();
         exit(0);
     } else if (pidSerf > 0) prodPID = pidSerf;
     else{
@@ -115,9 +137,9 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-
-
-
+    /**************************************************************************************/
+    /**                                      HAPPY END                                   **/
+    /**************************************************************************************/
 
     // pockame az vsichni skonci
     waitpid(consPID, NULL, 0);
@@ -127,6 +149,7 @@ int main(int argc, char *argv[])
     shm_unlink(shmKEY);
     shm_unlink(shmHACK);
     shm_unlink(shmSERF);
+    shm_unlink(shmMOLO);
     sem_unlink(SEMAPHORE1_NAME);
     sem_unlink(SEMAPHORE2_NAME);
 
@@ -182,7 +205,7 @@ int control(int argc, char *argv[])
     return 0;
 }
 
-void proc1() {
+void hack1() {
     sem_t *semaphore1;
     sem_t *semaphore2;
     semaphore1 = sem_open(SEMAPHORE1_NAME, O_RDWR);
@@ -201,19 +224,28 @@ void proc1() {
     close(hackID);
 
     for (int i=0; i<5; i++) {
-        sem_post(semaphore1);
-        sem_wait(semaphore2);
-        *shm = *shm + 1;
-        *hack = *hack + 1;
-        fprintf(stdout, "%d: HACKER %d: starts\n", *shm, *hack);
-        usleep(500);
+        pid_t pidHackFunc = fork();
+        if (pidHackFunc == 0) {
+            sem_post(semaphore1);
+            sem_wait(semaphore2);
+            *shm = *shm + 1;
+            *hack = *hack + 1;
+            fprintf(stdout, "%d: HACKER %d: starts\n", *shm, *hack);
+            exit(0);
+        }
+        else if(pidHackFunc > 0);
+        else{
+            //handle error
+            exit(1);
+        }
+        usleep((useconds_t)random() % 2000);
     }
 
     munmap(shm, shmSIZE);
     munmap(hack, shmSIZE);
 }
 
-void proc2() {
+void serf1() {
     sem_t *semaphore1;
     sem_t *semaphore2;
     semaphore1 = sem_open(SEMAPHORE1_NAME, O_RDWR);
@@ -233,11 +265,20 @@ void proc2() {
     close(serfID);
 
     for (int i=0; i<5; i++) {
-        sem_wait(semaphore1);
-        *serf = *serf + 1;
-        *shm = *shm + 1;
-        fprintf(stdout, "%d: SERF %d: starts\n", *shm, *serf);
-        sem_post(semaphore2);
+        pid_t pidSerfFunc = fork();
+        if (pidSerfFunc == 0){
+            sem_wait(semaphore1);
+            *serf = *serf + 1;
+            *shm = *shm + 1;
+            fprintf(stdout, "%d: SERF %d: starts\n", *shm, *serf);
+            sem_post(semaphore2);
+            exit(0);
+        }
+        else if(pidSerfFunc > 0);
+        else{
+            //handle error
+            exit(1);
+        }
         usleep((useconds_t)random() % 2000);
     }
 
