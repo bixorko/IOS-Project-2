105e7db88defba0b11aec020e6732e29362a8acf
PeterVinarcik <peter.vinarcik99@gmail.com>
2019-04-19 23:44:01 +0200

    IDK CO TO ROBI ALE NIECO ANO

--- main.c
+++ main.c
@@ -1,42 +1,134 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <semaphore.h>
-#include <sys/types.h>
-#include <pthread.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <signal.h>
+#include <sys/mman.h>
 
+//***************************************//
+//                SEMAPHORES             //
+//***************************************//
+
+#define SEMAPHORE1_NAME "/semaphore_synchronization1"
+#define SEMAPHORE2_NAME "/semaphore_synchronization2"
+
+//***************************************//
+//                FUNCTIONS              //
+//***************************************//
 int control(int argc, char *argv[]);
+void proc1();
+void proc2();
 
-sem_t mutex;
+//***************************************//
+//       DEFINES && GLOBAL VARIABLES     //
+//***************************************//
+sem_t *semaphore1;
+sem_t *semaphore2;
+#define shmSIZE sizeof(int)
+#define shmKEY "/ios-proj2"
+#define shmHACK "/ios-proj2-hack"
+#define shmSERF "/ios-proj2-serf"
 
 int main(int argc, char *argv[])
 {
+    setbuf(stdout,NULL);
+    setbuf(stderr,NULL);
+
     int controler = control(argc, argv);
-    if (controler == -1) {
+    if (controler == -1){
         return -1;
     }
 
-    if (sem_init(&mutex,0,0) == 0){
-        printf("Initialized mutex to 0!\n");
+    // create two locked semaphores
+    if ((semaphore1 = sem_open(SEMAPHORE1_NAME, O_CREAT | O_EXCL, 0666, 0)) == SEM_FAILED)
+    {
+        // handle error
+        return -1;
     }
+    sem_close(semaphore1);
 
-    pid_t hackers = fork();
-    if (hackers == 0){
-        printf("ChildOfHackers\n");
+    if ((semaphore2 = sem_open(SEMAPHORE2_NAME, O_CREAT | O_EXCL, 0666, 0)) == SEM_FAILED)
+    {
+        // handle error
+        return -1;
+    }
+    sem_close(semaphore2);
+
+    int pidSerf;
+    int pidHack;
+    int *shm;
+    int *hack;
+    int *serf;
+    int shmID;
+    int hackID;
+    int serfID;
+    pid_t consPID;
+    pid_t prodPID;
+
+    shmID = shm_open(shmKEY, O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR);
+    ftruncate(shmID, shmSIZE);
+    hackID = shm_open(shmHACK, O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR);
+    ftruncate(hackID, shmSIZE);
+    serfID = shm_open(shmSERF, O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR);
+    ftruncate(serfID, shmSIZE);
+    shm = (int*)mmap(NULL, shmSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shmID, 0);
+    hack = (int*)mmap(NULL, shmSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, hackID, 0);
+    serf = (int*)mmap(NULL, shmSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, serfID, 0);
+    close(shmID);
+    close(hackID);
+    close(serfID);
+    *shm = 0;
+    *hack = 0;
+    *serf = 0;
+    munmap(shm, shmSIZE);
+    munmap(hack, shmSIZE);
+    munmap(serf, shmSIZE);
+
+
+
+
+
+    pidHack = fork();
+    //HACKER PROCES
+    if (pidHack == 0) { // child
+        proc1();
         exit(0);
+    } else if (pidHack > 0) consPID = pidHack;
+    else{
+        //error handle
+        return -1;
     }
-    sem_close(&mutex);
 
-    printf("ParentOfHackers\n");
-
-    pid_t serfs = fork();
-    if (serfs == 0){
-        printf("ChildOfSerfs\n");
+    //SERF PROCES
+    pidSerf = fork();
+    if (pidSerf == 0) { // child
+        proc2();
         exit(0);
+    } else if (pidSerf > 0) prodPID = pidSerf;
+    else{
+        //error handle
+        return -1;
     }
-    sleep(2);
-    printf("ParentOfSerfs\n");
+
+
+
+
+
+    // pockame az vsichni skonci
+    waitpid(consPID, NULL, 0);
+    waitpid(prodPID, NULL, 0);
+
+    // zrusime zdroje
+    shm_unlink(shmKEY);
+    shm_unlink(shmHACK);
+    shm_unlink(shmSERF);
+    sem_unlink(SEMAPHORE1_NAME);
+    sem_unlink(SEMAPHORE2_NAME);
 
     return 0;
 }
@@ -88,4 +180,67 @@ int control(int argc, char *argv[])
     }
 
     return 0;
+}
+
+void proc1() {
+    sem_t *semaphore1;
+    sem_t *semaphore2;
+    semaphore1 = sem_open(SEMAPHORE1_NAME, O_RDWR);
+    semaphore2 = sem_open(SEMAPHORE2_NAME, O_RDWR);
+
+    int shmID;
+    int hackID;
+    int *shm;
+    int *hack;
+
+    shmID = shm_open(shmKEY, O_RDWR, S_IRUSR | S_IWUSR);
+    hackID = shm_open(shmHACK, O_RDWR, S_IRUSR | S_IWUSR);
+    shm = (int*)mmap(NULL, shmSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shmID, 0);
+    hack = (int*)mmap(NULL, shmSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, hackID, 0);
+    close(shmID);
+    close(hackID);
+
+    for (int i=0; i<5; i++) {
+        sem_post(semaphore1);
+        sem_wait(semaphore2);
+        *shm = *shm + 1;
+        *hack = *hack + 1;
+        fprintf(stdout, "%d: HACKER %d: starts\n", *shm, *hack);
+        usleep(500);
+    }
+
+    munmap(shm, shmSIZE);
+    munmap(hack, shmSIZE);
+}
+
+void proc2() {
+    sem_t *semaphore1;
+    sem_t *semaphore2;
+    semaphore1 = sem_open(SEMAPHORE1_NAME, O_RDWR);
+    semaphore2 = sem_open(SEMAPHORE2_NAME, O_RDWR);
+
+    int shmID;
+    int serfID;
+    int *shm;
+    int *serf;
+
+    // kontrola uspesnosti !!!
+    shmID = shm_open(shmKEY, O_RDWR, S_IRUSR | S_IWUSR);
+    serfID = shm_open(shmSERF, O_RDWR, S_IRUSR | S_IWUSR);
+    shm = (int*)mmap(NULL, shmSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shmID, 0);
+    serf = (int*)mmap(NULL, shmSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, serfID, 0);
+    close(shmID);
+    close(serfID);
+
+    for (int i=0; i<5; i++) {
+        sem_wait(semaphore1);
+        *serf = *serf + 1;
+        *shm = *shm + 1;
+        fprintf(stdout, "%d: SERF %d: starts\n", *shm, *serf);
+        sem_post(semaphore2);
+        usleep((useconds_t)random() % 2000);
+    }
+
+    munmap(shm, shmSIZE);
+    munmap(serf, shmSIZE);
 }
